7043918a5981664d5c224445d25c49d9f0c8bcc7
FIVE <cahhong@artfx.fr>
2024-05-01 12:58:58 +0200

    VOICE UPDATE -LOGS
    Adding Logs to the Voice Channel Update

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ bot_id = "asst_DDSMrYNpgfEulWI85Ccg882z"
 #serveur IDs
 serverid = [1159282148042350642, 945445171670171668]
 testid = [1159282148042350642]
+afkchannellist = [1126432480690446396]
 
 def context():
     global thread
@@ -362,11 +363,20 @@ async def on_voice_state_update(member, before, after):
         
         qdb.voiceactive(member.name)
         qdb.add(member.name, 15)
+        qlogs.info(f"{member.name} is connvected to a Voice Channel")
+    if after.channel.id in afkchannellist:
+        #USER CONNECTED TO AFK
+        if qdb.user_in_db(member.name) == 0:
+            qdb.add_user(member.name)
+        qlogs.info(f"{member.name} is detected AFK")
+        
+        qdb.voicestalled()
     if before.channel is not None and after.channel is None:
-        # User connected to a voice channel
+        # User disconnects
         if qdb.user_in_db(member.name) == 0:
             qdb.add_user(member.name)
         
         qdb.voicestalled(member.name)
+        qlogs.info(f"{member.name} is disconnected")
 
 bot.run(KEY_DISCORD)
