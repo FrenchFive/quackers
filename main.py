a29e380d89af253ad827bf4b7c7121a904028840
FIVE <chan.ahhong@gss-wks07.blackkitestudios.com>
2024-12-20 15:27:36 +0000

    Adding print for debugging

--- main.py
+++ main.py
@@ -542,6 +542,7 @@ async def on_voice_state_update(member, before, after):
 #WELCOME and GOODBYE
 @bot.event
 async def on_member_join(member):
+    print(f"{member.name} has joined the server")
     channel_id = "1225917633992462346"
     channel = bot.get_channel(channel_id)
     if channel:
@@ -549,6 +550,7 @@ async def on_member_join(member):
 
 @bot.event
 async def on_member_remove(member):
+    print(f"{member.name} has left the server")
     channel_id = "1225917633992462346"
     channel = bot.get_channel(channel_id)
     if channel:
