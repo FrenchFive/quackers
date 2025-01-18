d1dbb2b1e32e4223b98b5f3c783e4e031b093445
French Five <cahhong@artfx.fr>
2025-01-18 17:35:15 +0000

    Removing path bc it doesnt work with linux, linux uses a positional argurment instead of a var one

--- backend/src/consts.py
+++ backend/src/consts.py
@@ -1,6 +1,6 @@
 import os
 
-DATA_DIR: str = os.path.realpath(path=os.path.join(os.path.dirname(p=__file__), "../data"))
+DATA_DIR: str = os.path.realpath(os.path.join(os.path.dirname(p=__file__), "../data"))
 IMG_DIR: str = os.path.join(DATA_DIR, "imgs")
 FONT_DIR: str = os.path.join(DATA_DIR, "fonts")
-ROOT_DIR: str = os.path.realpath(path=os.path.join(os.path.dirname(p=__file__), "../.."))
+ROOT_DIR: str = os.path.realpath(os.path.join(os.path.dirname(p=__file__), "../.."))
