f41e37257862460f2419b64a3890617067e266e7
FIVE <cahhong@artfx.fr>
2025-01-26 23:22:53 +0000

    Making all the games field disappear if the general game is disabled

--- web/static/js/config-games.js
+++ web/static/js/config-games.js
@@ -1,3 +1,36 @@
+//GAME TOGGLE
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("game-toggle");
+    const section = document.getElementById("general-section");
+    const dice = document.getElementById("dice-block");
+    const rps = document.getElementById("rps-block");
+    const hball = document.getElementById("hball-block");
+    const bet = document.getElementById("bet-block");
+
+    toggle.addEventListener("change", () => {
+        if (toggle.checked) {
+            section.style.display = "block";
+            dice.style.display = "block";
+            rps.style.display = "block";
+            hball.style.display = "block";
+            bet.style.display = "block";
+        } else {
+            section.style.display = "none";
+            dice.style.display = "none";
+            rps.style.display = "none";
+            hball.style.display = "none";
+            bet.style.display = "none";
+        }
+    });
+
+    // Initialize the visibility based on the toggle's initial state
+    section.style.display = toggle.checked ? "block" : "none";
+    dice.style.display = toggle.checked ? "block" : "none";
+    rps.style.display = toggle.checked ? "block" : "none";
+    hball.style.display = toggle.checked ? "block" : "none";
+    bet.style.display = toggle.checked ? "block" : "none";
+});
+
 //BET TOGGLE
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("bet-toggle");
