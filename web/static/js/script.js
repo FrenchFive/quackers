c37bca5b2013105ce2cc635394d48e0ca0990004
FIVE <cahhong@artfx.fr>
2025-01-26 20:47:14 +0000

    Adding minor Disabling

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -2,3 +2,17 @@
 document.addEventListener('contextmenu', function (e) {
     e.preventDefault();
 });
+
+document.addEventListener('keydown', function (e) {
+    if (e.ctrlKey && (e.key === 'u' || e.key === 'U')) {
+        e.preventDefault();
+        alert('Viewing page source is disabled.');
+    }
+});
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+        e.preventDefault();
+        alert('Developer tools are disabled.');
+    }
+});
