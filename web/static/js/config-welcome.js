79dd2604ad11be351a9f76c1cb52ecafe8111164
FIVE <cahhong@artfx.fr>
2025-01-27 09:31:40 +0000

    Making all toggles work on WELCOME page

--- web/static/js/config-welcome.js
+++ web/static/js/config-welcome.js
@@ -65,3 +65,60 @@ document.addEventListener("DOMContentLoaded", () => {
     // Initialize the visibility based on the toggle's initial state
     section.style.display = toggle.checked ? "block" : "none";
 });
+
+//DISABLE Custom Welcome Message
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('wlc-cstm-msg-toggle');
+    const inputField = document.getElementById('wlc-cstm-msg-input');
+
+    toggle.addEventListener('change', () => {
+        if (toggle.checked) {
+            inputField.disabled = false;
+        } else {
+            inputField.disabled = true;
+        }
+    });
+
+    // Initial check
+    if (!toggle.checked) {
+        inputField.disabled = true;
+    }
+});
+
+//DISABLE Random Emote
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('bot-reaction-toggle');
+    const inputField = document.getElementById('bot-random-reaction-toggle');
+
+    toggle.addEventListener('change', () => {
+        if (toggle.checked) {
+            inputField.disabled = false;
+        } else {
+            inputField.disabled = true;
+        }
+    });
+
+    // Initial check
+    if (!toggle.checked) {
+        inputField.disabled = true;
+    }
+});
+
+//DISABLE Custom Welcome Message
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('gdb-msg-toggle');
+    const inputField = document.getElementById('gdb-msg-input');
+
+    toggle.addEventListener('change', () => {
+        if (toggle.checked) {
+            inputField.disabled = false;
+        } else {
+            inputField.disabled = true;
+        }
+    });
+
+    // Initial check
+    if (!toggle.checked) {
+        inputField.disabled = true;
+    }
+});
