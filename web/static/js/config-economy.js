67cb95349fb76b09b5684b26748dbfe90a80f617
FIVE <cahhong@artfx.fr>
2025-01-30 21:54:27 +0000

    Adding more exchange

--- web/static/js/config-economy.js
+++ web/static/js/config-economy.js
@@ -220,3 +220,55 @@ document.addEventListener('DOMContentLoaded', () => {
         inputField.disabled = true;
     }
 });
+
+//BUTTON SAVE ECO
+document.getElementById("btn-eco-eco").addEventListener("click", () => {
+    const saveButton = document.getElementById("btn-eco-eco");
+    saveButton.disabled = true;
+    saveButton.innerHTML = `
+        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    `;
+
+    const server_id = document.getElementById("server-id").value;
+
+    const data = [
+        { name: "eco", value: document.getElementById("coin-toggle").checked ? 1 : 0 },
+        { name: "eco_pss", value: document.getElementById("passive-toggle").value },
+        { name: "eco_pss_msg", value: document.getElementById("dm-toggle").checked ? 1 : 0 },
+        { name: "eco_pss_msg_value", value: document.getElementById("dm-toggle").value },
+        { name: "eco_pss_ch", value: document.getElementById("dm-toggle").checked ? 1 : 0 },
+        { name: "eco_pss_ch_value", value: document.getElementById("dm-toggle").value },
+        { name: "eco_pss_ch_hour", value: document.getElementById("dm-toggle").value },
+        { name: "eco_pss_ch_akf", value: document.getElementById("dm-toggle").checked ? 1 : 0 },
+        { name: "eco_pss_ch_akf_id", value: document.getElementById("dm-toggle").value },
+        { name: "eco_pss_cmd", value: document.getElementById("pss-cmd-toggle").checked ? 1 : 0 },
+        { name: "eco_pss_cmd_value", value: document.getElementById("pss-cmd-input").value },
+    ];
+
+    fetch("/save-config", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ server_id, data }),
+    })
+        .then(response => response.json())
+        .then(response => {
+            if (response.success) {
+                console.log(response.message);
+            } else {
+                alert("Failed to save changes.");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("An error occurred. Please try again.");
+        })
+        .finally(() => {
+            // Revert the button back to its original state
+            setTimeout(() => {
+                saveButton.disabled = false;
+                saveButton.innerHTML = "Save Changes";
+            }, 1000);
+        });
+});
