7b4820dfef40f929b08a589293660c666e774ecd
FIVE <cahhong@artfx.fr>
2025-02-02 18:35:06 +0000

    Making the AI page functionning

--- web/static/js/config-ai.js
+++ web/static/js/config-ai.js
@@ -33,3 +33,89 @@ document.addEventListener('DOMContentLoaded', () => {
         inputField.disabled = true;
     }
 });
+
+//BUTTON SAVE AI CHAT
+document.getElementById("btn-ai-chat").addEventListener("click", () => {
+    const saveButton = document.getElementById("btn-ai-chat");
+    saveButton.disabled = true;
+    saveButton.innerHTML = `
+        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    `;
+
+    const server_id = document.getElementById("server-id").value;
+
+    const data = [
+        { name: "ai_chat", value: document.getElementById("ai-chat-toggle").checked ? 1 : 0 },
+    ];
+
+    fetch("/save-config", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ server_id, data }),
+    })
+        .then(response => response.json())
+        .then(response => {
+            if (response.success) {
+                console.log(response.message);
+            } else {
+                alert("Failed to save changes.");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("An error occurred. Please try again.");
+        })
+        .finally(() => {
+            // Revert the button back to its original state
+            setTimeout(() => {
+                saveButton.disabled = false;
+                saveButton.innerHTML = "Save Changes";
+            }, 1000);
+        });
+});
+
+//BUTTON SAVE AI IMAGE
+document.getElementById("btn-ai-img").addEventListener("click", () => {
+    const saveButton = document.getElementById("btn-ai-img");
+    saveButton.disabled = true;
+    saveButton.innerHTML = `
+        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+    `;
+
+    const server_id = document.getElementById("server-id").value;
+
+    const data = [
+        { name: "ai_img", value: document.getElementById("imagine-toggle").checked ? 1 : 0 },
+        { name: "ai_img_pay", value: document.getElementById("ai-img-pay").checked ? 1 : 0 },
+        { name: "ai_img_pay_value", value: document.getElementById("ai-img-pay-value").value },
+    ];
+
+    fetch("/save-config", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ server_id, data }),
+    })
+        .then(response => response.json())
+        .then(response => {
+            if (response.success) {
+                console.log(response.message);
+            } else {
+                alert("Failed to save changes.");
+            }
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("An error occurred. Please try again.");
+        })
+        .finally(() => {
+            // Revert the button back to its original state
+            setTimeout(() => {
+                saveButton.disabled = false;
+                saveButton.innerHTML = "Save Changes";
+            }, 1000);
+        });
+});
